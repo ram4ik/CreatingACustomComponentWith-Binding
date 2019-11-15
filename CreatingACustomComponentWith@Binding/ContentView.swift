34bad0d73480449ff3f0d16310ef1479ab3dbcc5
Ramill Ibragimov <ramill_ibragimov@icloud.com>
2019-11-15 11:09:48 +0200

    Creating a custom component with @Binding

--- CreatingACustomComponentWith@Binding/ContentView.swift
+++ CreatingACustomComponentWith@Binding/ContentView.swift
@@ -8,12 +8,32 @@
 
 import SwiftUI
 
+struct PushButton: View {
+    let title: String
+    @Binding var isOn: Bool
+    
+    var onColors = [Color.red, Color.yellow]
+    var offColors = [Color(white: 0.6), Color(white: 0.4)]
+    
+    var body: some View {
+        Button(title) {
+            self.isOn.toggle()
+        }
+        .padding()
+        .background(LinearGradient(gradient: Gradient(colors: isOn ? onColors : offColors), startPoint: .top, endPoint: .bottomLeading))
+        .foregroundColor(.white)
+        .clipShape(Capsule())
+        .shadow(radius: isOn ? 0 : 5)
+    }
+}
+
 struct ContentView: View {
     @State private var rememberMe = false
     
     var body: some View {
-        Toggle(isOn: $rememberMe) {
-            Text("Remember Me")
+        VStack {
+            PushButton(title: "Remember Me", isOn: $rememberMe)
+            Text(rememberMe ? "On" : "Off")
         }
     }
 }
